5dc5dd2a9917d7d52395fa5e44fe52507ca954fe
Armocidautn <armocidaadrian@outlook.com>
2017-07-04 11:02:01 -0300

    Menu en un solo componente
    se cambio el menu en un solo componnte para poder usarlo en cualquier otro componente.

--- pizzeria/src/app/components/menu/menu.component.ts
+++ pizzeria/src/app/components/menu/menu.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { AutService } from '../../services/auth/aut.service';
+import { WsService } from '../../services/ws/ws.service';
+import { AuthHttp, AuthConfig, tokenNotExpired, JwtHelper } from 'angular2-jwt';
+import { FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-menu',
@@ -7,15 +13,56 @@ import { Router } from '@angular/router';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  private auth: AutService;
+public TipoAdministrador: boolean=false;
+ public TipoEncargado: boolean=false;
+ public TipoEmpleado: boolean=false;
+ public TipoCliente: boolean=false;
+  constructor(private router: Router) { 
+     
+     this.auth = new AutService(this.router);
+      
+        
+ if (this.auth.getToken().perfil == "Administrador")
+      {
+      this.TipoAdministrador=true;
+      }
+   
+  if (this.auth.getToken().perfil == "Encargado")
+      {
+      this.TipoEncargado=true;
+    }
+    if (this.auth.getToken().perfil == "Empleado")
+      {
+      this.TipoEmpleado=true;
+    }
+    if (this.auth.getToken().perfil == "Cliente")
+      {
+      this.TipoCliente=true;
+      }
 
-  constructor(private router: Router) { }
+
+
+ }
+
+  
 
   ngOnInit() {
   }
   salir()
   {
-    localStorage.setItem('token', null);
-    window.alert('Chauuuuuu!!!');
+    //localStorage.setItem('token', null);
+    localStorage.removeItem('token');
+    window.alert('Hasta Luego!!!');
     this.router.navigate(['/login']);
   }
+
 }
+
+
+
+
+  
+   
+
+
